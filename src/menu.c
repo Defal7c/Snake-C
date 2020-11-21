4a73a65a7e41c3ff5d99d8784ca07f0b8b62569b
Defal7 <mth.fe.a@protonmail.com>
2020-11-21 15:11:39 -0300

    Updated Options Menu

--- src/menu.c
+++ src/menu.c
@@ -2,12 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ncurses.h>
-
 #include "controls.h"
 
 
 int inMenu = 1;
-int OPT = 1;
+int OPT = 0;
 void MainMenu(void);
 void OptionsMenu(void);
 
@@ -17,46 +16,69 @@ void SelectMenu()
 {
     while(inMenu == 1)
     {
-        system("clear");
-        if(OPT == 0) inMenu = 0;
-        else if(OPT == 1) MainMenu();
+        clear();
+        if(OPT == 0) MainMenu();
+        else if(OPT == 1) inMenu = 0;
         else if(OPT == 2) OptionsMenu();
+        //else if(OPT == 4) Manual();
     }
 }
 
-int selected = 0;
-char * MENUS[] = { "START", "Options", "Manual", "Quit" };
-char ch;
+int selected = 1;
+char * MENUS[] = { "", "START", "Options", "Manual", "Quit" };
 
-//dimensions
-int y = 10;
-int x = 15;
+// Logo
+int logo_height = 8;
+int logo_width = 45;
+int logo_y = 0;
+int logo_x = 0;
+
+// main window
+int main_height = 6;
+int main_width = 15;
+int main_y = 10;
+int main_x = 15;
+
+//Cursor pos
+int cursorPOS[2] = {0, 0};
+
 
 void MainMenu()
 {
-    system("clear");
-    printw("-------------------------------------------\n");
-    printw("  ____      _          _       _           \n");
-    printw(" / ___|___ | |__  _ __(_)_ __ | |__   __ _ \n");
-    printw("| |   / _ `| '_ `| '__| | '_ `| '_ ` / _` |\n");
-    printw("| |__| (_) | |_) | |  | | | | | | | | (_| |\n");
-    printw("`____`____/|_.__/|_|  |_|_| |_|_| |_|`__,_|\n");
-    printw("                             Simulator 2020\n");
-    printw("-------------------------------------------\n");
-
-    for(int i = 0; i < 4; i++)
+    WINDOW * logo = newwin(logo_height, logo_width,logo_y,logo_x);
+    box(logo, 0,0);
+    mvwprintw(logo,1,1,"  ____      _          _       _           ");
+    mvwprintw(logo,2,1," / ___|___ | |__  _ __(_)_ __ | |__   __ _ ");
+    mvwprintw(logo,3,1,"| |   / _ `| '_ `| '__| | '_ `| '_ ` / _` |");
+    mvwprintw(logo,4,1,"| |__| (_) | |_) | |  | | | | | | | | (_| |");
+    mvwprintw(logo,5,1,"`____`____/|_.__/|_|  |_|_| |_|_| |_|`__,_|");
+    mvwprintw(logo,6,1,"                             Simulator 2020");
+    wrefresh(logo);
+    
+    WINDOW * mainwindow = newwin(main_height, main_width, main_y, main_x);
+    box(mainwindow, 0,0);
+    
+    for(int i = 1; i < 5; i++)
     {
-        mvaddch(y + i,x,'[');
-        if(i == selected) mvaddch(y + i,x+1,'*'); else mvaddch(y + i,x+1,'\0');
-        mvaddch(y + i,x+2,']');
-        mvprintw(y + i,x+5,"%s", MENUS[i]);
+        mvwaddch(mainwindow, i,1,'[');
+        if(i == selected) 
+        { 
+            cursorPOS[0] == i;
+            cursorPOS[1] == 1;
+            mvwaddch(mainwindow, i, 2,'*'); 
+        }else{ 
+            mvwaddch(mainwindow, i,2,'\0');
+        }
+        mvwaddch(mainwindow, i ,3,']');
+        mvwprintw(mainwindow, i,6,"%s", MENUS[i]);
     }
-    // ch = getch();
+    int ch = wgetch(mainwindow);
+    if(ch == ENTER) OPT = selected;
+    if(ch == DOWN) { if(selected != 4) selected++; }
+    if(ch == UP) { if(selected != 0) selected--; }
 
-    if(ch == 's') { if(selected != 3) selected++; ch = 'A'; }
-    if(ch == 'w') { if(selected != 0) selected--; ch = 'A'; }
-    //if(ch==0x0A) OPT = selected;
-    getch();
+    wrefresh(logo);
+    wrefresh(mainwindow);
 }
 
 
@@ -65,32 +87,64 @@ int MAXOPT = 1;
 char * OPTIONS[] = {"Disable Colors", "Go Back"};
 int arrow;
 
+//Options Window
+int optmenu_height = 6;
+int optmenu_width = 20;
+int optmenu_x = 10;
+int optmenu_y = 15;
+
+//Options Logo
+int opt_height = 8;
+int opt_width = 45;
+
 void OptionsMenu()
 {
-    system("clear");
-    printf("-------------------------------------------\n");
-    printf("  ___        _   _                 \n");
-    printf(" / _ ` _ __ | |_(_) ___  _ __  ___ \n");
-    printf("| | | | '_ `| __| |/ _ `| '_ `/ __|\n");
-    printf("| |_| | |_) | |_| | (_) | | | `__ `\n");
-    printf("` ___/| .__/` __|_|`___/|_| |_|___/\n");
-    printf("      |_|                          \n");
-    printf("-------------------------------------------\n");
-    printf("\n");
+    WINDOW * opt = newwin(opt_height, opt_width, logo_y, logo_x);
+    box(opt, 0,0);
+    //mvwprintw(opt,1,1,"%d", sel);
+    mvwprintw(opt,1,1,"  ___        _   _                 ");
+    mvwprintw(opt,2,1," / _ ` _ __ | |_(_) ___  _ __  ___ ");
+    mvwprintw(opt,3,1,"| | | | '_ `| __| |/ _ `| '_ `/ __|");
+    mvwprintw(opt,4,1,"| |_| | |_) | |_| | (_) | | | `__ `");
+    mvwprintw(opt,5,1,"` ___/| .__/` __|_|`___/|_| |_|___/");
+    mvwprintw(opt,6,1,"      |_|                          ");
+    wrefresh(opt);
 
+    WINDOW * optmenu = newwin(optmenu_height, optmenu_width, optmenu_y, optmenu_x);
+    box(optmenu,0,0);
     //this is the best code I've ever written in my entire life.
     for(int i = 0; i < 2; i++)
     {
-        printf("[");
-        if(i == sel) printf("*"); else printf(" ");
-        printf("] - ");
-        printf("%s\n", OPTIONS[i]);
+        mvwaddch(optmenu,i+1,1,'[');
+        if(i == sel)
+        {
+            cursorPOS[0] = i+1;
+            cursorPOS[1] = 2;
+            mvwaddch(optmenu, i+1,2,'*'); 
+        }else mvwaddch(optmenu, i+1,2, '\0');
+        mvwaddch(optmenu,i+1,3,']');
+        mvwprintw(optmenu,i+1,6,"%s", OPTIONS[i]);
     }
-    scanf("%s", mv);
+    move(cursorPOS[0], cursorPOS[1]);
+    int ch = wgetch(optmenu);
 
-    if(strcmp(mv, DOWN) == 0)
+    if(ch == ENTER)
     {
-        if(sel != MAXOPT) sel++;
-    }else if(strcmp(mv, UP) == 0) if(sel != 0) sel--;
-
+        if(sel == 1) OPT = 0;
+    }
+    switch(ch)
+    {
+        case DOWN:
+            if(sel == MAXOPT) break;
+            sel++;
+            break;
+        case UP:
+            if(sel == 0) break;
+            sel--;
+            break;
+        default:
+            break;
+    }
+    wrefresh(opt);
+    wrefresh(optmenu);
 }
